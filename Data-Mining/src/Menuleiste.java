74b79a0ebe53f37395b1769380c73e5ec1b3e9f8
patric <masterpatt@web.de>
2013-02-14 15:05:56 +0100

    CSV-Dateien können nun eingelesen werden!!! :)

--- Data-Mining/src/Menuleiste.java
+++ Data-Mining/src/Menuleiste.java
@@ -1,5 +1,13 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 import java.util.Vector;
 
 import javax.swing.JFileChooser;
@@ -53,9 +61,47 @@ class Menuleiste extends JMenuBar {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
+				String zeile, zwischen;
+				int z = 0;
+				Vector kopfzeile = new Vector();
+				Vector daten = new Vector();
+				Vector speicher;
 				JFileChooser tabellenimport = new JFileChooser();
 				tabellenimport.setFileFilter(new Dateifilter());
-				tabellenimport.showOpenDialog(oberflaeche);
+				if (tabellenimport.showOpenDialog(oberflaeche) 
+					== JFileChooser.APPROVE_OPTION) {
+					try {
+						BufferedReader in = new BufferedReader(
+						new FileReader(tabellenimport.getSelectedFile()));
+						try {
+							while ((zeile = in.readLine()) != null) {
+								z++;
+								StringTokenizer zerteiler = new StringTokenizer(zeile, "\n,");
+								speicher = new Vector();
+								while (zerteiler.hasMoreTokens()) {
+									if (z == 1) {
+										zwischen = zerteiler.nextToken();
+										kopfzeile.add(zwischen);
+										System.out.println("Kopfzeile: " + zwischen);
+									}
+									else {
+										zwischen = zerteiler.nextToken();
+										speicher.add(zwischen);
+										System.out.println(z + ". Datensatz: " + zwischen);
+									}
+								}
+								if (z > 1) {
+									daten.add(speicher);
+								}
+							oberflaeche.datenaktualisieren(daten, kopfzeile);
+							}
+						} catch (IOException e2) {
+							e2.printStackTrace();
+						}
+					} catch (FileNotFoundException e1) {
+						e1.printStackTrace();
+					}
+				}
 			}
 		});
 		tabelle.add(importieren);
